a1cae87a59ec64638698b062d2bb32b591907955
Louie D <louie.delrosario97@gmail.com>
2026-06-04 14:39:54 -0700

    NorthwindTraders Pt.1 complete

--- src/main/com/pluralsight/NorthwindTraders.java
+++ src/main/com/pluralsight/NorthwindTraders.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class NorthwindTraders
 {
     public static void main(String[] args)
@@ -9,17 +14,22 @@ public class NorthwindTraders
         // DataSource Setup
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
-        dataSource.setUsername("username");
-        dataSource.setPassword("password");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yearup26");
 
         // Query the Database
         String sql = "SELECT ProductName, UnitPrice FROM products";
 
+        // Try-with-resources
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement= connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery())
+        {
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString("ProductName"));
+            }
+        } catch (SQLException e) { e.printStackTrace(); }
     }
-
-
-
-
-
 }
 
